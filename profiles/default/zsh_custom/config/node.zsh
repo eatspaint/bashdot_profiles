65985c38a8fd5be00b6cca0105fa0e8ed4f16c64
eatspaint <cef2010@gmail.com>
2022-03-17 18:45:14 -0700

    patch nvm config

--- profiles/default/zsh_custom/config/node.zsh
+++ profiles/default/zsh_custom/config/node.zsh
@@ -1,7 +1,6 @@
 # NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/bash_completion" ] && \. "/usr/local/opt/nvm/bash_completion"  # This loads nvm bash_completion
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # NPM memory fix
 export NODE_OPTIONS="--max_old_space_size=4096"
